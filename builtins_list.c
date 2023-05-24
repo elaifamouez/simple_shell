750f91eb60f3d04838cefc10d4859cbf4a313419
elaifa <mouezelaifa@gmail.com>
2023-05-24 09:52:10 -0700

    SIMPLE SHELL

--- builtins_list.c
+++ builtins_list.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 /**
  * builtins_list - search for match and execute the associate builtin
  * @data: struct for the program's data
@@ -8,28 +7,24 @@
  **/
 int builtins_list(data_of_program *data)
 {
-	int iterator;
-	builtins options[] = {
-		{"exit", builtin_exit},
-		{"help", builtin_help},
-		{"cd", builtin_cd},
-		{"alias", builtin_alias},
-		{"env", builtin_env},
-		{"setenv", builtin_set_env},
-		{"unsetenv", builtin_unset_env},
-		{NULL, NULL}
-	};
+int iterator;
 
-/*walk through the structure*/
-	for (iterator = 0; options[iterator].builtin != NULL; iterator++)
-	{
-/*if there is a match between the given command and a builtin,*/
-		if (str_compare(options[iterator].builtin, data->command_name, 0))
-		{
-/*execute the function, and return the return value of the function*/
-			return (options[iterator].function(data));
-		}
-/*if there is no match return -1 */
-	}
-	return (-1);
+builtins options[] = {
+{"exit", builtin_exit},
+{"help", builtin_help},
+{"cd", builtin_cd},
+{"alias", builtin_alias},
+{"env", builtin_env},
+{"setenv", builtin_set_env},
+{"unsetenv", builtin_unset_env},
+{NULL, NULL}
+};
+for (iterator = 0; options[iterator].builtin != NULL; iterator++)
+{
+if (str_compare(options[iterator].builtin, data->command_name, 0))
+{
+return (options[iterator].function(data));
+}
+}
+return (-1);
 }
