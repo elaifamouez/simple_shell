0d5bd2b4abae25a349be92255ccd204e24328dc3
elaifa <mouezelaifa@gmail.com>
2023-05-24 12:22:19 -0700

    SIMPLE SHELL

--- expand_variables.c
+++ expand_variables.c
@@ -14,7 +14,7 @@ if (data->input_line == NULL)
 return;
 buffer_add(line, data->input_line);
 for (i = 0; line[i]; i++)
-if (line[i] == '$' && line[i + 1] == '#')
+if (line[i + 1] == '#')
 line[i] = '\0';
 else if (line[i] == '$' && line[i + 1] == '?')
 {
